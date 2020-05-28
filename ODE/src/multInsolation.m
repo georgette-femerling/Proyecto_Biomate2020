9e8574169a29a162482602386bca38c252d20878
georgette-femerling <63737044+georgette-femerling@users.noreply.github.com>
2020-05-28 13:32:26 -0500

    Add files via upload

---
+++ ODE/src/multInsolation.m
@@ -0,0 +1,22 @@
+function simulation = multInsolation(period, di, de, y0, beta, gamma)
+    Nperiods = period/(di+de);
+    simulation = [];
+    for n=[1:1:Nperiods]        
+        % Inicia periodo de aislamiento        
+        tspan = [0:1:di];
+        [ t, Insolation ] = ode45( @(t,SIR) odefun(t, SIR, ...
+                            20, beta, gamma), tspan, y0 );        
+        % Termina periodo de aislamiento
+        y0 = Insolation(end, : );
+        % Inicia periodo de exposicion        
+        tspan = [0:1:de];
+        [ t, Exposition ] = ode45( @(t,SIR) odefun(t, SIR, ...
+                            0, beta, gamma), tspan, y0 );
+        % Termina periodo de exposicion
+        y0 = Exposition(end, : );
+        % Periodo completo
+        ciclo = [Exposition; Insolation];
+        % Alamacenando ciclo
+        simulation = [simulation; ciclo];
+    end
+end
